06b0ae761806e4f15a37234e4cddd508c7f2902e
Jhon Anthony Valenzuela Armas <N00471400@upn.pe>
2026-09-22 22:12:38 -0500

    req 2 y 6 funcion obligatoria y validacion de codigo

--- Program.cs
+++ Program.cs
@@ -15,6 +15,9 @@ static void Main(string[] args)
                 {
                     case "1":
                         Console.WriteLine("1. Registro de solicitudes");
+                        //requerimiento 2 y 6 solicitar y validar datos del usuario para registrar una solicitud
+                        string codigo=leerTextoObligatorio("Ingrese el código del estudiante: (Minimo 4 caracteres)", 4);
+                        Console.WriteLine($"Código ingresado: {codigo}");
                         break;
                     case "2":
                         Console.WriteLine("2. Ver solicitudes registradas");
@@ -36,6 +39,21 @@ static void Main(string[] args)
             Console.WriteLine("1. Registro de solicitudes");
             Console.WriteLine("2. Ver solicitudes registradas");
             Console.WriteLine("3. Salir del programa");
+        }
+        //funcion 6 con retorno, validar que el texto ingresado por el usuario cumpla con un mínimo de caracteres
+        static string leerTextoObligatorio(string mensaje, int longitudMinima)
+        {
+            string texto = "";
+            do
+            {
+                Console.WriteLine(mensaje);
+                texto = Console.ReadLine() ?? "";
+                if (string.IsNullOrWhiteSpace(texto) || texto.Length < longitudMinima)
+                {
+                    Console.WriteLine($"¡Error! El texto debe tener al menos {longitudMinima} caracteres. Intente nuevamente.");
+                }
+            } while (string.IsNullOrWhiteSpace(texto) || texto.Length < longitudMinima);
+            return texto;
         }
     }
 }
